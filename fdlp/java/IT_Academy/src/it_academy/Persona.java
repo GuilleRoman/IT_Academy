bec265badde75b55d3042b86ccec9320f93aadc1
juanjo2708 <juanjo2708@gmail.com>
2021-03-24 18:02:39 +0100

    primer commit del repositori

---
+++ fdlp/java/IT_Academy/src/it_academy/Persona.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it_academy;
+
+/**
+ *
+ * @author juanj
+ */
+public class Persona {
+    private String nom;
+    private int edat;
+    private String caracter;
+    private Boolean bonaGent;
+
+    public Persona(String nom, int edat, String caracter) {
+        this.nom = nom;
+        this.edat = edat;
+        this.caracter = caracter;
+        this.bonaGent=true;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getEdat() {
+        return edat;
+    }
+
+    public void setEdat(int edat) {
+        this.edat = edat;
+    }
+
+    public String getCaracter() {
+        return caracter;
+    }
+
+    public void setCaracter(String caracter) {
+        this.caracter = caracter;
+    }
+
+    public Boolean getBonaGent() {
+        return bonaGent;
+    }
+
+    public void setBonaGent(Boolean bonaGent) {
+        this.bonaGent = bonaGent;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
