3fcb00804fba08f97a7704423e295d1592e0cc13
juanjo2708 <juanjo2708@gmail.com>
2021-04-06 11:54:00 +0200

    m07exercici2

---
+++ fdlp/java/M7exercici4/src/m7exercici4/M7exercici4.java
@@ -0,0 +1,21 @@
+/*
+ * IT ACADEMY  .
+ * Fonaments de la programació -POO-
+ * Juan José Campos Caballero.
+ */
+package m7exercici4;
+
+/**
+ *
+ * @author juanj
+ */
+public class M7exercici4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
