ce3dd37ae5d6d21294f7c2c695ea48486a2ba717
root <juanjo2708@gmail.com>
2021-09-17 12:01:31 +0200

    Big laptop act.

---
+++ actualitzaRemot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo Actualitzant el repositori GitHub https://github.com/juanjo2708/IT_Academy!
+git add .
+git status
+git commit 
+git status
+git push
+echo Procés d actualització remot finalitzat.
---
+++ actualitzarLocal.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo Actualitzant repositori https://github.com/juanjo2708/IT_Academy!
+git pull https://github.com/juanjo2708/IT_Academy
+echo Procés d actualització finalitzat.
