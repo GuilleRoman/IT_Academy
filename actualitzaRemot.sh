32ff4d706f36d30a60af7fd5a9a5be061a117f06
root <juanjo2708@gmail.com>
2021-09-17 12:35:26 +0200

    Big laptop

--- actualitzaRemot.sh
+++ actualitzaRemot.sh
@@ -2,7 +2,7 @@
 echo Actualitzant el repositori GitHub https://github.com/juanjo2708/IT_Academy!
 git add .
 git status
-git commit 
+git commit -m "Big laptop " 
 git status
 git push
 echo Procés d actualització remot finalitzat.
